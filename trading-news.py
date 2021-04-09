379bacb3d8ad2f4c47675c76a22045f9b6180a77
Remington Anton Amalanathan <remingtonanton@gmail.com>
2021-04-09 03:18:41 +0100

    Update trading-news.py
    Fixing CFD bug + adding tickers.

--- trading-news.py
+++ trading-news.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from time import sleep
 import re
+import sys
 
 # Initialise browser and specify login details
-browser = webdriver.Chrome(executable_path=r"YOUR PATH TO \chromedriver.exe")
+browser = webdriver.Chrome(executable_path=r"D:\Projects\Trading\chromedriver.exe")
 browser.implicitly_wait(10)
 username = "USERNAME"
-password = "EMAIL"
+password = "PASSWORD"
 
 
 # Open Trading212 and login
@@ -18,25 +19,32 @@ username_field = browser.find_element_by_name("login[password]")
 username_field.send_keys(password)
 login_button = browser.find_element_by_class_name("button-login")
 login_button.click()
-sleep(6) # wait for page to l load html
+sleep(6) # wait for page to l   oad html
 
 #Annoying pop up on Trading212 Invest
 browser.maximize_window()
 
-continue_button = browser.find_element_by_class_name("custom-button")
-continue_button.click()
 
 # Get page HTML source
+cfd = "accountTradingType: 'CFD',"
 source = browser.page_source
+
+
+#Find out of invest or CFD 
+if cfd in source:
+    browser.quit() 
+    sys.exit("CFD Page")
 
 
 #Annoying trading212 pop up 
 continue_button = browser.find_element_by_class_name("custom-button")
 continue_button.click()
 
+
 # Find regex, for any stock
 stock_regex = re.compile(r'<td data-column-id="name" class="name">[a-zA-Z ]+')
 holdings = stock_regex.findall(source)
+
 
 # Extract name of stock from html
 stocks = []
@@ -44,8 +52,9 @@ for holding in holdings:
     stock = holding.replace('<td data-column-id="name" class="name">', "")
     stocks.append(stock)
 
-
+# Google search each stock
 for stock in stocks:
 
     browser.execute_script('''window.open("https://www.google.com/search?q={} news","_blank");'''.format(stock))
+
 
